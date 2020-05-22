3bdfcb03a1bfaad565927a4ecf2faa44c78c54dc
noisleahcim@gmail.com <noisleahcim@gmail.com>
2020-05-22 13:46:30 +0300

    Flask app

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Ahallan")
+from flask import Flask
+from bs4 import BeautifulSoup
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "Ariul is mad gay!"
+
+def today_historical_events():
+    wiki_url = 'https://en.wikipedia.org/wiki/Wikipedia:On_this_day/Today'
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
